09f8098aac3510d6b29a90896e595346b2e865db
Raphi <raphaelfetrat@protonmail.com>
2023-12-16 08:24:41 -0500

    added theater files

--- raphi-messing-around/js/javascript-helloworld.js
+++ raphi-messing-around/js/javascript-helloworld.js
@@ -23,3 +23,85 @@ function foo(bar) {
     alert(bar);
 }
 
+let dogSits = true;
+
+let dogGetsTreat;
+
+if (dogSits) {
+    dogGetsTreat = true;
+}
+
+
+
+
+// if (roll === 1 || roll === 6) {
+//     console.log("You win");
+// } else if ( roll <= 3) {
+//     console.log("Roll again.")
+// } else {
+//     console.log("You lose.")
+// }
+
+
+
+
+function testSwitch(size) {
+    switch (size) {
+        case "small":
+            alert("This chair is too small.");
+            break;
+        case "medium":
+            alert("This chair is just right.");
+            break;
+        case "large":
+            alert("This chair is too big.");
+            break;
+        default:
+            alert("This chair is an unknown size.");
+    }
+}
+
+let funCheck = true && null && 5;
+
+let statusThing = "2" === 2;
+
+let statusAsString = String(!statusThing);
+
+let boolZeroComparison = 0 === false;
+
+console.log(funCheck);
+
+console.log(statusThing);
+
+console.log(statusAsString);
+
+console.log(boolZeroComparison);
+
+console.log(dogGetsTreat);
+
+
+function getMembershipPerks(mLevel) {
+    /* Your switch statement should be written below this line.*/
+    mLevel = mLevel.toLowerCase()  // forcing all getMembershipPerks() arguments .toLowerCase() so UPPERCASE arguments (e.g. "GOLD" or "Gold") don't cause errors.
+    if (mLevel === "gold" || mLevel === "silver" || mLevel === "bronze") {
+        switch (mLevel) {
+            case "gold":
+                console.log("Unlimited Free Plays\n" + "VIP Room Access");
+                break;
+            case "silver":
+                console.log("VIP Room Access");
+                break;
+            case "bronze":
+                console.log("VIP Room Access on Weekdays");
+            //   break;
+            // default:
+            //   console.log("Invalid membership level");
+        }
+    }
+    else {
+        console.log("Invalid membership level");
+    }
+}
+
+getMembershipPerks("asdf");
+/* After you make changes, you will need to click the Run button on the upper right. Auto-updating preview has been turned off for this Codepen. */
