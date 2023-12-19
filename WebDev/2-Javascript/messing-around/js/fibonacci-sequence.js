b9271432080a310a38ccdca401c3d8bdac04184e
Raphi <raphaelfetrat@protonmail.com>
2023-12-18 23:52:24 -0500

    created fibonacci solutions

--- WebDev/2-Javascript/messing-around/js/fibonacci-sequence.js
+++ WebDev/2-Javascript/messing-around/js/fibonacci-sequence.js
@@ -1,16 +1,100 @@
-function printFibonacci(n) {
+function printFibonacciWhileLoop(count) {
     let a = 0;
     let b = 1;
-    let fib = 0;
-    let count = 0;
-    while (count < n) {
-       fib = a + b;
-       console.log(`Fibonacci is currently: ${a}`);
+    let i = 0;
+    while (i < count) {
+       let next = a + b;
+       console.log(`Fibonacci: ${a}`);
        console.log(`Count: ${count}`);
        a = b;
-       b = fib;
-       count++;
+       b = next;
+       i++;
     }
 }
 
-printFibonacci(100);
+
+function printFibonacciForLoop(count) {
+    let a = 0;
+    let b = 1;
+    for (let i = 0; i < count; i++) {
+        let next = a + b;
+        console.log(`Fibonacci: ${a}`);
+        console.log(`Count: ${i}`);
+        a = b;
+        b = next;
+    }
+}
+
+
+function fibonacciUntilLimit() {
+    let highestNumberToPrint = prompt("What is the highest number you want to print?");
+    let a = 0;
+    let b = 1;
+    let count = 0;
+    while (a <= highestNumberToPrint) {
+        let next = a + b;
+        console.log(`Fibonacci: ${a}`);
+        console.log(`Count: ${count}`);
+        a = b;
+        b = next;
+        count++;
+    }
+}
+
+
+function fibonacciEvenNumbers() {
+    let highestNumberToPrint = prompt("What is the highest number you want to go to?");
+    let a = 0;
+    let b = 1;
+    while (a <= highestNumberToPrint) {
+        let next = a + b;
+        let evenOrOdd = (a % 2);
+        if (evenOrOdd === 0) {
+            console.log(`Fibonacci: ${a}`);
+        }
+        a = b;
+        b = next;
+    }
+}
+
+
+function fibonacciSumUp() {
+    let numbersToAdd = prompt("How many numbers do you want to add?");
+    let a = 0;
+    let b = 1;
+    let sum = 0;
+
+    for (let i = 0; i < numbersToAdd; i++) {
+        let next = a + b;
+        sum += next;
+        a = b;
+        b = next;
+    }
+
+    console.log(sum);
+
+}
+
+
+function fibonacciIsNumberPartOf() {
+    let numberToCheck = prompt("What number do you want to check?");
+    let a = 0;
+    let b = 1;
+    let count = 0;
+    let foundMatch;
+
+    while (a !== numberToCheck) {
+        if (a === numberToCheck) {
+
+        }
+
+
+        let next = a + b;
+        console.log(`Fibonacci: ${a}`);
+        console.log(`Count: ${count}`);
+        a = b;
+        b = next;
+        count++;
+
+    }
+}
