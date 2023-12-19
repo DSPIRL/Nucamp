41cdd651ef3980374cb28d611978d17536c58c3a
Raphi <raphaelfetrat@protonmail.com>
2023-12-19 12:21:39 -0500

    added array.js

---
+++ WebDev/2-Javascript/messing-around/js/array.js
@@ -0,0 +1,17 @@
+let firstArray = [1,2,3,4,5];
+
+let secondArray = [6,7,8,9,0];
+
+console.log(firstArray);
+
+console.log(firstArray[1]);
+
+console.log(firstArray.length);
+
+console.log(firstArray.indexOf(5));
+
+console.log(firstArray.includes(5));
+
+let thirdArray = firstArray.concat(secondArray);
+
+console.log(thirdArray);
