e76fdf1578ed3d706faac1dc03af57ea215afe04
Raphi <raphaelfetrat@protonmail.com>
2023-12-24 15:25:21 -0500

    Completed challenge

--- WebDev/2-Javascript/color-guessing-game/js/color-guessing-game.js
+++ WebDev/2-Javascript/color-guessing-game/js/color-guessing-game.js
@@ -1,6 +1,9 @@
+// Global variables
 const COLORS_ARRAY = ['gold', 'cyan', 'blue', 'gray', 'green', 'magenta', 'orange', 'red', 'white', 'yellow'];
 let GUESSED_COLORS = [];
 
+
+// Function to start game
 function runGame() {
     let numTries = 0;
     let guess = '';
@@ -10,8 +13,7 @@ function runGame() {
     console.log(target);
 
     do {
-        guess = prompt('I am thinking of one of these colors:\n\n' + COLORS_ARRAY.sort().join(", ") +
-            '\n\nWhat color am I thinking of?\n').toLowerCase();
+        guess = prompt('I am thinking of one of these colors:\n\n' + COLORS_ARRAY.sort().join(", ") + '\n\nWhat color am I thinking of?\n').toLowerCase();
 
 
         if (guess === null || guess === '') {
@@ -24,29 +26,39 @@ function runGame() {
         correct = checkGuess(guess, target);
     } while (!correct);
 
-    alert(`Congratulations!\n\nYou have guessed the correct color! It took you ${numTries} tries (${GUESSED_COLORS}) to guess it.`)
+    alert(`Congratulations!\n\nYou have guessed the correct color! It took you ${numTries} tries (${GUESSED_COLORS.join(', ')}) to guess it.`)
 
 }
 
+
+// Check if guess is correct and return true if it is correct
 function checkGuess(guess, target) {
     let correct = false;
     if (!COLORS_ARRAY.includes(guess)) {
         alert('Your color is not included in the options.');
     } else if (guess < target) {
-        changeColor(guess);
         alert('Your guess is lower alphabetically than the color.');
 
     } else if (guess > target) {
-        changeColor(guess);
         alert('Your guess is higher alphabetically than the color.');
     } else {
         correct = true;
     }
 
+    changeColor(guess);
     GUESSED_COLORS.push(guess);
     return correct;
 }
 
+
+// If color is correct, this will change the background color to the guessed color
 function changeColor(guess) {
     document.body.style.backgroundColor = guess;
 }
+
+
+
+
+// document.querySelector('button').setAttribute( 'disabled', '');
+
+document.getElementById('start-game');
