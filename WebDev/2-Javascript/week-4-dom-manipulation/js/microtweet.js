7f60115f9c21d808f4ea46d0a9df029613b1de34
Raphi <raphaelfetrat@protonmail.com>
2024-01-05 23:44:24 -0500

    Add new HTML and JS files in Week-4 Javascript section
    New HTML and JS files have been added for week-4 Javascript curriculum. These files cover various mouse events and event listeners, DOM manipulation techniques, including changing background and text colors, and different interactive elements like Microtwitter and Colorful Patterns projects.

---
+++ WebDev/2-Javascript/week-4-dom-manipulation/js/microtweet.js
@@ -0,0 +1,10 @@
+const inputEl = document.querySelector("input");
+// Add your event listener here
+document.addEventListener("input", fixLength);
+function fixLength() {
+    // Your code here
+
+    function myFunction() {
+        document.getElementById("demo").innerHTML = "Hello World";
+    }
+}
