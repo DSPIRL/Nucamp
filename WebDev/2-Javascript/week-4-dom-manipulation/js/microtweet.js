70ca84cce42a48b77342a3c090546edc5075a79a
raphi <raphaelfetrat@protonmail.com>
2024-01-06 07:12:05 -0500

    added nucampsite folder to JS class

--- WebDev/2-Javascript/week-4-dom-manipulation/js/microtweet.js
+++ WebDev/2-Javascript/week-4-dom-manipulation/js/microtweet.js
@@ -1,10 +1,16 @@
 const inputEl = document.querySelector("input");
+
 // Add your event listener here
-document.addEventListener("input", fixLength);
+document.addEventListener("keyup", fixLength);
+
+// You can also add a maxlength attribute to the field off the bat in the HTML and it will prevent more than 12 characters from existing in the field.
+// inputEl.setAttribute('maxlength', '12');
+
 function fixLength() {
     // Your code here
+    const fieldValue = inputEl.value;
 
-    function myFunction() {
-        document.getElementById("demo").innerHTML = "Hello World";
+    if (fieldValue.length > 12) {
+        inputEl.value = fieldValue.substring(0, 12);
     }
 }
