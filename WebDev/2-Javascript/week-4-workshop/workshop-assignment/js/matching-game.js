93c3d1c8982d3378844f6f4a5c43a823a8129801
Raphi <raphaelfetrat@protonmail.com>
2024-01-06 21:17:32 -0500

    Add new matching game files for week 4 workshop
    The JavaScript Week 4 workshop now includes a new HTML and JavaScript file for a matching game. The game has different difficulty levels and the interface allows the user to select a game difficulty. Moreover, the JavaScript file controls the game logic including winning streaks, attempt counts, and game difficulty level changes.

---
+++ WebDev/2-Javascript/week-4-workshop/workshop-assignment/js/matching-game.js
@@ -0,0 +1,99 @@
+let difficultyLevel = changeDifficulty();
+let winStreak = 0;
+let attemptCount = 0;
+const difficultyButton = document.getElementById('changeButton');
+const theLeftSide = document.getElementById('leftSide');
+const theRightSide = document.getElementById('rightSide');
+
+function newGame() {
+    clearGame();
+    difficultyLevel = changeDifficulty();
+    generateGame();
+    winStreak = 0;
+}
+
+function generateGame() {
+    for (let i = 0; i < difficultyLevel; i++) {
+        let face = document.createElement('img');
+        face.src = "img/new-smile.png";
+        face.width = 60;
+        face.className = "guessItem";
+        let randomTop = Math.floor(Math.random() * 400 + 1);
+        let randomLeft = Math.floor(Math.random() * 400 + 1);
+        face.style.top = randomTop + 'px';
+        face.style.left = randomLeft + 'px';
+
+        theLeftSide.appendChild(face);
+    }
+
+    const leftSideImages = theLeftSide.cloneNode(true);
+    leftSideImages.removeChild(leftSideImages.lastChild);
+
+    for (let i = 0; i < leftSideImages.childNodes.length;) {
+        theRightSide.appendChild(leftSideImages.firstChild);
+
+    }
+
+    // theRightSide.appendChild(leftSideImages);
+    theLeftSide.lastChild.addEventListener("click", nextLevel);
+    theLeftSide.addEventListener("click", gameOver);
+}
+
+function nextLevel(event) {
+    winStreak++;
+
+    while (theLeftSide.firstChild) {
+        theLeftSide.removeChild(theLeftSide.firstChild);
+    }
+
+    while (theRightSide.firstChild) {
+        theRightSide.removeChild(theRightSide.firstChild);
+    }
+
+    event.stopPropagation();
+    difficultyLevel += difficultyLevel;
+    generateGame();
+}
+
+function clearGame() {
+    theLeftSide.lastChild.removeEventListener("click", nextLevel);
+    theLeftSide.removeEventListener("click", gameOver);
+
+    while (theLeftSide.firstChild) {
+        theLeftSide.removeChild(theLeftSide.firstChild);
+    }
+
+    while (theRightSide.firstChild) {
+        theRightSide.removeChild(theRightSide.firstChild);
+    }
+}
+
+function gameOver() {
+    attemptCount++;
+    alert(`Game over! You had a winning streak of ${winStreak} games.`);
+    winStreak = 0;
+    newGame();
+}
+
+function changeDifficulty() {
+    let difficulty = getDifficulty();
+    let selection = 0;
+
+    if (difficulty === 'easy') {
+        selection = 3;
+    } else if (difficulty === 'medium') {
+        selection = 5;
+    } else if (difficulty === 'hard') {
+        selection = 7;
+    }
+
+    return selection;
+}
+
+function getDifficulty() {
+    const difficultyDdl = document.getElementById('difficulty-selector');
+    return difficultyDdl.value;
+}
+
+window.addEventListener('load', generateGame);
+difficultyButton.addEventListener('click', newGame);
